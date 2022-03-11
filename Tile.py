91e2b792ff0b6e7397670f2c0962fa6054f5fb4f
Jacob Stegemann <jacob-stegemann@gmx.de>
2022-03-11 18:08:23 +0100

    Fix init and randomize bugs
    - When initializing, [edges] should be an optional parameter.
    - When randomizing, [EdgeType]s should be used, instead of ints.

--- Tile.py
+++ Tile.py
@@ -2,7 +2,7 @@ from enum import Enum
 from typing import List
 from random import randint
 
-        
+
 class EdgeType(Enum):
     Gras = 0
     Forest = 1
@@ -13,9 +13,9 @@ class EdgeType(Enum):
     WaterTrain = 6 # Omit first
 
 class Tile:
-    def __init__(self, edges: List[EdgeType]) -> None:    
+    def __init__(self, edges: List[EdgeType] = None) -> None:
         if edges is not None:
-            self.edges = edges # Clockwise 
+            self.edges = edges # Clockwise
         else:
             self.edges: List[EdgeType] = []
             self.randomizeEdges()
@@ -23,9 +23,11 @@ class Tile:
     def randomizeEdges(self):
         enumOrder = len(list(EdgeType))
         for i in range(6):
-            self.edges.append(randint(enumOrder))
+            self.edges.append(EdgeType(randint(0, enumOrder - 1)))
 
-    
+        return self
+
+
     def rotate(self, n):
         """
         Rotates edge types clockwise by n
@@ -33,5 +35,5 @@ class Tile:
         newEdges = []
         for i in range(6):
             newEdges.append(self.edges[i - (-n % 6)])
-            
+
         self.edges = newEdges
