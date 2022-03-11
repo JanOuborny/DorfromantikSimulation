939a90bfb0d33e27529f291ef1607fb491ef1b23
Jan Ouborny <jan.ouborny@pm.me>
2022-03-11 14:57:29 +0100

    Add test for tile rotate

---
+++ test/tile_test.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from Tile import Tile
+
+class TileTest(TestCase):
+    
+    def test_rotate(self):
+        # Arrange
+        tile = Tile([1, 2, 3, 4, 5, 6])
+
+        # Act
+        tile.rotate(3)
+
+        # Assert
+        self.assertEqual(tile.edges[3], 1)
