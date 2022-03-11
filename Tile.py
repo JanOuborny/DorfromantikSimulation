8e149fe624bb9080a9fbb329d9cb4dc7cc57e3a4
Jan Ouborny <jan.ouborny@pm.me>
2022-03-11 14:57:45 +0100

    Various changes in Tile and World

--- Tile.py
+++ Tile.py
@@ -1,19 +1,6 @@
-
-import enum
-
 from enum import Enum
 from typing import List
 from random import randint
-
-class Tile:
-    def __init__(self) -> None:      
-        self.edges: List[EdgeType] = []
-        self.randomizeEdges()
-
-    def randomizeEdges(self):
-        enumOrder = len(list(EdgeType))
-        for i in range(6):
-            self.edges.append(randint(enumOrder))
 
         
 class EdgeType(Enum):
@@ -24,3 +11,27 @@ class EdgeType(Enum):
     Water = 4
     Train = 5
     WaterTrain = 6 # Omit first
+
+class Tile:
+    def __init__(self, edges: List[EdgeType]) -> None:    
+        if edges is not None:
+            self.edges = edges # Clockwise 
+        else:
+            self.edges: List[EdgeType] = []
+            self.randomizeEdges()
+
+    def randomizeEdges(self):
+        enumOrder = len(list(EdgeType))
+        for i in range(6):
+            self.edges.append(randint(enumOrder))
+
+    
+    def rotate(self, n):
+        """
+        Rotates edge types clockwise by n
+        """
+        newEdges = []
+        for i in range(6):
+            newEdges.append(self.edges[i - (-n % 6)])
+            
+        self.edges = newEdges
--- World.py
+++ World.py
@@ -2,15 +2,25 @@ from Tile import Tile
 from typing import List
 
 class World:
+    MAX_SIZE = 1001
+    MID_POINT = 500
 
     def __init__(self) -> None:
-        self.map: List[List[Tile]]= [] 
+        self.map: List[List[Tile]] = [[None for j in range(World.MAX_SIZE)] for i in range(World.MAX_SIZE)]
 
-    # Inserts tile and returns score. -1 if insertion is not possible
-    def insertTile(tile) -> int:
+
+
+    def insertTileAt(self, tile, x, y) -> int:
+        """
+        Inserts tile and returns score. 
+        Returns -1 if insertion is not possible.
+        """
+
         pass
 
-    def getPossiblePositions(tile):
+    def getPossiblePositions(self, tile):
         pass
 
+    def getAdjacentTiles(self, x, y):
+        pass
     
