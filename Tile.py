ecd50ef56946b978d8185c2decd3b6e888a1ba08
Jan Ouborny <jan.ouborny@pm.me>
2022-03-11 14:02:08 +0100

    Initial commit

---
+++ Tile.py
@@ -0,0 +1,26 @@
+
+import enum
+
+from enum import Enum
+from typing import List
+from random import randint
+
+class Tile:
+    def __init__(self) -> None:      
+        self.edges: List[EdgeType] = []
+        self.randomizeEdges()
+
+    def randomizeEdges(self):
+        enumOrder = len(list(EdgeType))
+        for i in range(6):
+            self.edges.append(randint(enumOrder))
+
+        
+class EdgeType(Enum):
+    Gras = 0
+    Forest = 1
+    Cornfield = 2
+    Houses = 3
+    Water = 4
+    Train = 5
+    WaterTrain = 6 # Omit first
---
+++ World.py
@@ -0,0 +1,16 @@
+from Tile import Tile
+from typing import List
+
+class World:
+
+    def __init__(self) -> None:
+        self.map: List[List[Tile]]= [] 
+
+    # Inserts tile and returns score. -1 if insertion is not possible
+    def insertTile(tile) -> int:
+        pass
+
+    def getPossiblePositions(tile):
+        pass
+
+    
