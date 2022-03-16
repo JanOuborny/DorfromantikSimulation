62a07c7c5835e0948904802fc1ce8dbadb222a3d
Jacob Stegemann <jacob-stegemann@gmx.de>
2022-03-16 02:00:23 +0100

    Fix Renderer y-down vs y-up confusion

--- test/renderer_test.py
+++ test/renderer_test.py
@@ -1,4 +1,6 @@
 from unittest import TestCase
+from typing import List
+import random
 
 from world import World
 from tile import Tile, EdgeType
@@ -25,7 +27,21 @@ class MockWorldBig(World):
     MAX_SIZE = 50
 
     def __init__(self):
-        self.map = [[Tile() for i in range(MockWorldBig.MAX_SIZE)] for j in range(MockWorldBig.MAX_SIZE)]
+        self.map = [[Tile() if random.random() < 0.6 else None for i in range(MockWorldBig.MAX_SIZE)] for j in range(MockWorldBig.MAX_SIZE)]
+
+class MockWorldFitting(World):
+    MAX_SIZE = 2
+
+    def __init__(self):
+        # A [Tile] with houses for the [indices] and grass elsewhere.
+        T = lambda indices: Tile(edges=[
+            EdgeType.Houses if i in indices else EdgeType.Gras for i in range(6)
+        ])
+
+        self.map = [
+            [T([1, 2]), T([4])],
+            [T([5]), T([])]
+        ]
 
 class TestRenderer(TestCase):
     def test_renderSmall(self):
@@ -40,6 +56,18 @@ class TestRenderer(TestCase):
         # Act
         renderer.render('test/render_small.svg')
 
+    def test_renderLabels(self):
+        """
+        should render a small svg with labels indicating the coordinates and
+        edge indices.
+        """
+
+        # Arrange
+        world = MockWorldSmall()
+        renderer = Renderer(world)
+        # Act
+        renderer.render('test/render_labels.svg', addLabels=True)
+
     def test_renderBig(self):
         """
         should render the .svg even for big maps.
@@ -50,4 +78,14 @@ class TestRenderer(TestCase):
         renderer = Renderer(world)
         # Act
         renderer.render('test/render_big.svg')
-        # Assert
+
+    def test_renderFitting(self):
+        """
+        should render a connected spot of houses, ranging over three 3 tiles.
+        """
+
+        # Arrange
+        world = MockWorldFitting()
+        renderer = Renderer(world)
+        # Act
+        renderer.render('test/render_fitting.svg')
